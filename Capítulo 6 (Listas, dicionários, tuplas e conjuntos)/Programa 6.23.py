8fa43b6434809147e86b1cc0f116aa9dbabefddc
Fabrício Vale <fabriciovale18@gmail.com>
2021-11-23 21:19:10 -0300

    Exercícios resolvidos

---
+++ Capítulo 6 (Listas, dicionários, tuplas e conjuntos)/Programa 6.23.py
@@ -0,0 +1,13 @@
+"""
+    Programa 6.23 - Exemplo de dicionário sem valor padrão
+"""
+
+d = {}
+
+for letra in 'abracadabra':
+    if letra in d:
+        d[letra] = d[letra] + 1
+    else:
+        d[letra] = 1
+
+print(d)
