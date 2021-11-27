d745e43f3dd5e1f122b9d0e18ceb38eb13aaa829
Fabrício Vale <fabriciovale18@gmail.com>
2021-11-26 21:38:48 -0300

    Exercícios resolvidos

--- Capítulo 7 (Trabalhando com Strings)/ex006.py
+++ Capítulo 7 (Trabalhando com Strings)/ex006.py
@@ -8,18 +8,4 @@
 Resultado: AAAACCAA
 """
 
-string_a = 'AATTCGAA'
-string_a = list(string_a)
-
-string_b = 'TG'
-string_c = 'AC'
-
-
-for c in range(0, (len(string_a)-1)):
-    if string_b[0] == string_a[c]:
-        string_a[c] = string_c[0]
-    elif string_b[1] == string_a[c]:
-        string_a[c] = string_c[1]
-
-resultado = ''.join(string_a)
-print(resultado)
+print(f'{5678:d}')
